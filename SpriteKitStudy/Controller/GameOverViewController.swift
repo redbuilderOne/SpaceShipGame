83b9e97f9fb03f537c841469366b0ad496a8a614
redbuilderOne <forsaken2492@yandex.ru>
2022-07-15 17:43:17 +0300

    gameovervc

---
+++ SpriteKitStudy/Controller/GameOverViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameOverViewController.swift
+//  SpriteKitStudy
+//
+//  Created by Дмитрий Скворцов on 15.07.2022.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func resetButton(_ sender: UIButton) {
+    }
+
+}
